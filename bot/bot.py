570d1d7b1f6986b0b9ae08102cd748d9f494eddb
Drekken <54035775+DrekkSama@users.noreply.github.com>
2023-08-14 19:35:00 -0400

    Initial commit

---
+++ bot/bot.py
@@ -0,0 +1,37 @@
+from sc2.bot_ai import BotAI, Race
+from sc2.data import Result
+
+
+class CompetitiveBot(BotAI):
+    NAME: str = "CompetitiveBot"
+    """This bot's name"""
+
+    RACE: Race = Race.Terran
+    """This bot's Starcraft 2 race.
+    Options are:
+        Race.Terran
+        Race.Zerg
+        Race.Protoss
+        Race.Random
+    """
+
+    async def on_start(self):
+        """
+        This code runs once at the start of the game
+        Do things here before the game starts
+        """
+        print("Game started")
+
+    async def on_step(self, iteration: int):
+        """
+        This code runs continually throughout the game
+        Populate this function with whatever your bot should do!
+        """
+        pass
+
+    async def on_end(self, result: Result):
+        """
+        This code runs once at the end of the game
+        Do things here after the game ends
+        """
+        print("Game ended.")
