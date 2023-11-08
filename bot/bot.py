a3551d3f35be2ef047b5be9abb7af16b1853224b
Drekken <drekken.pownz@protonmail.me>
2023-11-08 08:59:14 -0500

    Added bot can make probes

--- bot/bot.py
+++ bot/bot.py
@@ -1,9 +1,11 @@
 from sc2.bot_ai import BotAI, Race
 from sc2.data import Result
+from sc2.ids.unit_typeid import UnitTypeId
+from sc2.ids.ability_id import AbilityId
 
 
 class CompetitiveBot(BotAI):
-    NAME: str = "CompetitiveBot"
+    NAME: str = "DragonBot"
     """This bot's name"""
 
     RACE: Race = Race.Protoss
@@ -21,14 +23,25 @@ class CompetitiveBot(BotAI):
         Do things here before the game starts
         """
         print("Game started")
-
+        
+        
     async def on_step(self, iteration: int):
         """
         This code runs continually throughout the game
         Populate this function with whatever your bot should do!
         """
-        pass
-
+        
+        print(f"this is my bot in iteration {iteration}") #print iteration
+        
+        for loop_nexus in self.workers:
+            if self.can_afford(UnitTypeId.PROBE):
+                self.townhalls.ready.random.train(UnitTypeId.PROBE)
+        
+                # Add break statement here if you only want to train one
+            else:
+                # Can't afford probes anymore
+                break
+            
     async def on_end(self, result: Result):
         """
         This code runs once at the end of the game
