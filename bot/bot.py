396cbfe602f54de91e47e78737ea1a253727f475
Drekken <drekken.pownz@protonmail.com>
2024-02-07 17:40:11 -0500

    Add upgrade and buff imports, and refactor probe training logic

--- bot/bot.py
+++ bot/bot.py
@@ -2,6 +2,8 @@ from sc2.bot_ai import BotAI, Race
 from sc2.data import Result
 from sc2.ids.unit_typeid import UnitTypeId
 from sc2.ids.ability_id import AbilityId
+from sc2.ids.upgrade_id import UpgradeId
+from sc2.ids.buff_id import BuffId
 
 
 class CompetitiveBot(BotAI):
@@ -28,18 +30,31 @@ class CompetitiveBot(BotAI):
     async def on_step(self, iteration: int):
         """
         This code runs continually throughout the game
-        Populate this function with whatever your bot should do!
         """
         """
         print(f"this is my bot in iteration {iteration}") #print iteration
         """
         await self.distribute_workers() #puts idle workers to work
 
+        nexus = self.townhalls.ready.random
+
+        # Build a pylon if we are low on supply
         if self.supply_left < 2 and self.already_pending(UnitTypeId.PYLON) == 0:
             if self.can_afford(UnitTypeId.PYLON):
-                nexus = self.townhalls.ready.random
                 await self.build(UnitTypeId.PYLON, near=nexus)
             return
+        
+        for loop_nexus in self.workers:
+            nexus_list = self.structures(UnitTypeId.NEXUS).ready.idle  # Get list of idle nexuses
+            if self.can_afford(UnitTypeId.PROBE) and self.workers.amount < 16 and nexus_list.exists: # Need to look at after expanding, only works for 1 nexus
+                self.townhalls.ready.random.train(UnitTypeId.PROBE)
+        
+                # Add break statement here if you only want to train one
+            else:
+                # Can't afford probes anymore
+                break
+            
+        
 
         if self.can_afford(UnitTypeId.GATEWAY) and self.structures(UnitTypeId.GATEWAY).amount < 2:
             pylon = self.structures(UnitTypeId.PYLON).ready
@@ -55,22 +70,13 @@ class CompetitiveBot(BotAI):
             for gateway in gateways:
                 if len(zealots) < 12:
                     gateway.train(UnitTypeId.ZEALOT)
-                    print("Zealot built")
+                    
                 
         if len(zealots) == 12:
             for zealot in zealots:
                 zealot.attack(self.enemy_start_locations[0])
 
-        for loop_nexus in self.workers:
-            nexus_list = self.structures(UnitTypeId.NEXUS).ready.idle  # Get list of idle nexuses
-            if self.can_afford(UnitTypeId.PROBE) and self.workers.amount < 16 and nexus_list.exists: # Need to look at after expanding, only works for 1 nexus
-                self.townhalls.ready.random.train(UnitTypeId.PROBE)
         
-                # Add break statement here if you only want to train one
-            else:
-                # Can't afford probes anymore
-                break
-            
             
     
     async def on_end(self, result: Result):
